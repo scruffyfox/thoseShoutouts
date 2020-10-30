c6ff33e19909a3ac8b4126c33bb6cf694a8ccd71
thoseEyes <65393659+thoseEyes@users.noreply.github.com>
2020-10-30 10:11:04 +0000

    Displaying correct username in chat for manual shoutouts

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ function onMessageHandler(target, context, msg, self) {
                 username = username.substring(1)
             }
 
-            shoutout(username, messageGenerator.custom(context['display-name']))
+            shoutout(username, messageGenerator.custom(username))
         }
     }
 
